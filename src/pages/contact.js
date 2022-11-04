e2d20fc4aa01916b5ad017274b741f8cb0abf6f8
Michael Uyanna <crackmastermike@gmail.com>
2022-11-04 16:00:02 +0100

    update build files

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,20 @@
 export default function Contact() {
+    const placeholderText = "Send me a message and I'll reply you as soon as possible..."
     return (
-        <div className="input-container">
-            <input type="text" id="first_name" placeholder="First Name" />
-            <input type="text" id="last_name" placeholder="Last Name" />
-            <input type="email" id="email" placeholder="Email" />
-            <textarea id="message"></textarea>
-            <button id="btn_submit"></button>
+        <div className="contact">
+            <h1>Contact Me</h1>
+            <p>Hi there, contact me to ask me about anything you have in mind.</p>
+            <div className="input-container">
+                <label htmlFor="first_name">First Name</label>
+                <input type="text" id="first_name" placeholder="Enter your first name" />
+                <label htmlFor="last_name">Last Name</label>
+                <input type="text" id="last_name" placeholder="Enter your last name" />
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" placeholder="Yourname@email.com" />
+                <label htmlFor="message">Message</label>
+                <textarea id="message" placeholder={placeholderText}></textarea>
+                <button id="btn_submit"></button>
+            </div>
         </div>
     )
 }
