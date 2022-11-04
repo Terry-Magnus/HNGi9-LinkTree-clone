dcee8efda08a41126db0849610cf1a9f938036d2
Michael Uyanna <crackmastermike@gmail.com>
2022-11-04 07:57:53 +0100

    feat: added react router syntax in index,js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from './App';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
-
 
