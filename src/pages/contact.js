af50e50f9a27543db9b3e4d173d597c406f3a80b
Michael Uyanna <crackmastermike@gmail.com>
2022-11-04 15:28:49 +0100

    feat: created contact page structure

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,11 @@
 export default function Contact() {
     return (
         <div className="input-container">
-            <input type="text" id="first_name" />
-            <input type="text" id="last_name" />
+            <input type="text" id="first_name" placeholder="First Name" />
+            <input type="text" id="last_name" placeholder="Last Name" />
+            <input type="email" id="email" placeholder="Email" />
+            <textarea id="message"></textarea>
+            <button id="btn_submit"></button>
         </div>
     )
 }
