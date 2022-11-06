0ad75c9988e6918c173d9357156d51b38b9edc06
Michael Uyanna <crackmastermike@gmail.com>
2022-11-06 07:17:34 +0100

    update routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Contact from './pages/contact';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
